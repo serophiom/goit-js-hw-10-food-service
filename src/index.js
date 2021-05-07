f157ffe7314c848f82174d8abfb9103d79f619f7
serophiom <serophiom@gmail.com>
2021-05-07 22:32:00 +0300

    создает разметку с помощью шаблона

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+import foodCardTpl from './templates/food-cards.hbs';
+import menu from './menu.json';
+const foodSection = document.querySelector('.js-menu');
+
+const foodCardsMarkup = createFoodCardsMarkup(menu);
+
+foodSection.insertAdjacentHTML('beforeend', foodCardsMarkup);
+
+function createFoodCardsMarkup(menu) {
+    return foodCardTpl(menu);
+}
