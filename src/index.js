1f70aaba75bb00d2362f713898a83afa1c24139f
serophiom <serophiom@gmail.com>
2021-05-09 13:15:07 +0300

    добавляет список ингредиентов и удаляет все console.log

--- src/index.js
+++ src/index.js
@@ -1,53 +1,39 @@
 import './sass/main.scss';
 import foodCardTpl from './templates/food-cards.hbs';
 import menu from './menu.json';
-
-const foodSection = document.querySelector('.js-menu');
-const checkBoxRef = document.querySelector('#theme-switch-toggle');
-console.log(checkBoxRef);
-const bodyRef = document.querySelector('body');
 const Theme = {
         LIGHT: 'light-theme',
         DARK: 'dark-theme',
       };
-localStorage.setItem('theme', JSON.stringify(Theme));
-const themeStorage = localStorage.getItem('theme');
-const parsedThemeStorage = JSON.parse(themeStorage);
-console.log(parsedThemeStorage);
-
-console.log(localStorage);
-console.log(bodyRef);
-console.log(checkBoxRef);
-
-bodyRef.classList.add('light-theme');
-
-
-
+const foodSection = document.querySelector('.js-menu');
+const checkBoxRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body'); 
 const foodCardsMarkup = createFoodCardsMarkup(menu);
-
-checkBoxRef.addEventListener('change', changeMainTheme)
-
-function changeMainTheme () {
-    // if (bodyRef.class === '') {
-    //     bodyRef.class = '';
-    //     bodyRef.classList.add = 'ligth-theme';
-    // } 
-    // bodyRef.classList.add('dark-theme');
-    if (bodyRef.className === 'light-theme') {
-        bodyRef.className = '';
-        bodyRef.className = 'dark-theme';
-        console.log(bodyRef.className);
-    } 
-    if (bodyRef.className === 'dark-theme') {
-        bodyRef.className = '';
-        bodyRef.className = 'light-theme';
-        console.log(bodyRef.className);
-    }
-    // console.log(bodyRef);
-}
 
 foodSection.insertAdjacentHTML('beforeend', foodCardsMarkup);
 
+checkedCurrentTheme ();
+
+checkBoxRef.addEventListener('change', changeMainTheme);
+
 function createFoodCardsMarkup(menu) {
     return foodCardTpl(menu);
+}
+
+function changeMainTheme () {
+    if (checkBoxRef.checked) {
+        bodyRef.className = Theme.DARK;
+        localStorage.setItem('current-theme', `${Theme.DARK}`);
+        return;
+    }
+    bodyRef.className = Theme.LIGHT;
+    localStorage.setItem('current-theme', `${Theme.LIGHT}`);
+}
+
+function checkedCurrentTheme () {
+    const themeStorage = localStorage.getItem('current-theme');
+    if (themeStorage === `${Theme.DARK}`) {
+        bodyRef.className = Theme.DARK;
+        checkBoxRef.checked = true;
+    }
 }
